bd78c7d92b84016ce7433fe50016d7bc440cabc8
andrewc1204 <andrewc@neyasystems.com>
2014-11-10 11:25:29 -0500

    modified create_sym_links.sh to search for all files named dot_* in env/ and to create symlinks in the home directory automatically

--- bin/create_sym_links.sh
+++ bin/create_sym_links.sh
@@ -1,8 +1,11 @@
-echo -n "Creating sym link ~/.inputrc to ~/ubuntu_setup/env/dot_inputrc..."
-if [ -f /home/andrewc1204/.inputrc ];
-then
-  echo "file already exists."
-else
-  ln -s ~/ubuntu_setup/env/dot_inputrc ~/.inputrc
-  echo "done."
-fi
+for file in $( ls $HOME/ubuntu_setup/env/dot_* ); do
+  filename=${file/ubuntu_setup\/env\/dot_/.}
+  echo -n "Creating sym link $filename to $file..."
+  if [ -f $filename ];
+  then
+    echo "file already exists."
+  else
+    ln -s $file $filename
+    echo "done."
+  fi
+done
