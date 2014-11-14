f68a46832a0033abea15426fc31bf0c6783d559e
andrewc1204 <andrewc@neyasystems.com>
2014-11-14 17:25:32 -0500

    added overwrite option for dot_* files instead of never writing them if soft link exists; added sshxc alias to bash.local; added type plugin on to dot_vimrc for NERD commenter plugin

--- bin/create_sym_links.sh
+++ bin/create_sym_links.sh
@@ -1,11 +1,17 @@
 for file in $( ls $HOME/ubuntu_setup/env/dot_* ); do
-  filename=${file/ubuntu_setup\/env\/dot_/.}
-  echo -n "Creating sym link $filename to $file..."
-  if [ -f $filename ];
-  then
-    echo "file already exists."
-  else
-    ln -s $file $filename
-    echo "done."
-  fi
+    filename=${file/ubuntu_setup\/env\/dot_/.}
+    create_file=false
+    echo -n "Creating sym link $filename to $file..."
+    if [[ -f $filename ]]; then
+        read -p "File already exists. Overwrite file [Y/n]?:" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            ln -s -f $file filename 
+            echo "Overwrote existing softlink for $filename"
+        fi 
+    else
+        ln -s $file $filename
+        echo "Created softlink for $filename"
+    fi
+    echo "...done."
 done
