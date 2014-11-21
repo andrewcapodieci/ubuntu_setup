bcb9c494fd51724ac5780b57de71fd2cae6a3c24
andrewc1204 <andrewc@neyasystems.com>
2014-11-21 09:14:10 -0500

    changed ~ to /home/andrewc1204 in setup.sh; added function to auto quit vim when NERDTree is the only window...also put all autocmd's in their own section

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
-~/ubuntu_setup/bin/install_packages.sh
-~/ubuntu_setup/bin/git-config.sh
-~/ubuntu_setup/bin/create_sym_links.sh
-~/ubuntu_setup/bin/config_bash.sh
+$HOME/ubuntu_setup/bin/install_packages.sh
+$HOME/ubuntu_setup/bin/git-config.sh
+$HOME/ubuntu_setup/bin/create_sym_links.sh
+$HOME/ubuntu_setup/bin/config_bash.sh
