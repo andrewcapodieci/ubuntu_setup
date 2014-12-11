06301fa8e5e700e4253a47660bc77116b0b4fdad
andrewc1204 <andrewc@neyasystems.com>
2014-12-11 09:03:55 -0500

    added pidgin to install_packages.sh

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y vim screen git gitk indicator-multiload indicator-cpufreq exuberant-ctags yakuake clang libclang-dev
+sudo apt-get install -y vim screen git gitk indicator-multiload indicator-cpufreq exuberant-ctags yakuake clang libclang-dev pidgin
