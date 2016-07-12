8d946a37f1b367083963bdac53c238c4225d055a
andrewcapodieci <andrewc@neyasystems.com>
2016-07-12 12:54:29 -0400

    added setup for core files

---
+++ bin/configure_ubuntu.sh
@@ -0,0 +1,1 @@
+sudo echo "/tmp/cores/core.%e.%p.%h.%t" > /proc/sys/kernel/core_pattern
--- bin/setup.sh
+++ bin/setup.sh
@@ -5,4 +5,4 @@ $HOME/ubuntu_setup/bin/config_bash.sh
 $HOME/ubuntu_setup/bin/install_vim_plugins.sh
 $HOME/ubuntu_setup/bin/configure_applications.sh
 $HOME/ubuntu_setup/bin/configure_unity.sh
-
+$HOME/ubuntu_setup/bin/configure_ubuntu.sh
