7e5ecfb31ed1efa486362df105ef03ee43dc93c1
andrewcapodieci <andrewc@neyasystems.com>
2017-03-20 10:50:17 -0400

    fixed spelling of bash config script in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 $HOME/ubuntu_setup/scripts/install_packages.sh
 $HOME/ubuntu_setup/scripts/git-config.sh
 $HOME/ubuntu_setup/scripts/create_sym_links.sh
-$HOME/ubuntu_setup/scripts/config_bash.sh
+$HOME/ubuntu_setup/scripts/configure_bash.sh
 $HOME/ubuntu_setup/scripts/install_vim_plugins.sh
 $HOME/ubuntu_setup/scripts/configure_applications.sh
 $HOME/ubuntu_setup/scripts/configure_unity.sh
